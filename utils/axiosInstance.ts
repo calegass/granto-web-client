9c26ab9ee5f311255e3e1025178579b21ab61961
matheus <matheuscm7@hotmail.com>
2024-07-05 19:36:30 -0300

    fix: change url back

--- utils/axiosInstance.ts
+++ utils/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:8000/',
+  baseURL: 'https://web-production-435e.up.railway.app/',
   withCredentials: true, // para permitir o envio de cookies (se necessário)
 });
 
